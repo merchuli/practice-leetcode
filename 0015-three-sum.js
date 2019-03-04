1a8f6ea359a77256fa3514cd4ec9b708e8ac65ef
merchuli <merchu.liang@gmail.com>
2019-03-05 00:01:43 +0800

    Rename file name

--- 0015-three-sum.js
+++ 0015-three-sum.js
@@ -6,7 +6,7 @@
  * @author Merchuli
 
     Problem
-    1. Three Sum
+    15. Three Sum
 
     Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0
     Find all unique triplets in the array which gives the sum of zero.
