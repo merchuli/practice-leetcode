589e0358e43f07531bf1b28beeff29bd086d4960
Merchuli <30795240+merchuli@users.noreply.github.com>
2021-12-01 23:36:41 +0800

    Create 0000-sample.js

---
+++ 0000-sample.js
@@ -0,0 +1,32 @@
+/**
+ * @Problem license LeetCode
+
+ * @Problem Link https://leetcode.com/problems/sample-problem/
+
+ * @author Merchuli
+
+	Description of the problem
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var getLength = function(nums) {
+    /** Sample code */
+
+    return nums.length;
+};
+
+const testcase1 = [0,1,0,3,12];
+const testcase2 = [1,2];
+const testcase3 = [3,0,9,1,0];
+const testcase4 = [0,0,0,1];
+
+let testcase = testcase1;
+getLength(testcase);
+
+// Comment
+// Runtime: xx ms, faster than xx.xx% of JavaScript online submissions for Sample Problem.
+// Memory Usage: xx MB, less than xx.xx% of JavaScript online submissions for Sample Problem.
